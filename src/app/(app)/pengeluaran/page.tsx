8b8e880751ebec0e05c57cb8b2a948b296c68daf
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-05-12 01:06:12 +0700

    pengeluaran

---
+++ src/app/(app)/pengeluaran/page.tsx
@@ -0,0 +1,9 @@
+import { PengeluaranDataTable } from "@/components/data-table-pengeluaran";
+
+export default function PengeluaranPage(){
+  return (
+    <div className="container mx-auto py-8">
+      <PengeluaranDataTable />
+    </div>
+  );
+}
