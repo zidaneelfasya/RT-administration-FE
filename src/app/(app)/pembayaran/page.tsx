f0d41d3bd22d443d71e2df30ec8841c9dadb712f
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-05-12 01:06:02 +0700

    pembayaran

---
+++ src/app/(app)/pembayaran/page.tsx
@@ -0,0 +1,11 @@
+import { PembayaranDataTable } from "@/components/data-table-pembayaran";
+
+
+
+export default function PembayaranPage() {
+  return (
+    <div className="container mx-auto py-4">
+      <PembayaranDataTable />
+    </div>
+  );
+}
