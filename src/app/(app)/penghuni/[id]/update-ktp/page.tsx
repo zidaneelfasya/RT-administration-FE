95ce9a360256d3e1676483a3f7405edd34688c2a
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-05-12 01:06:31 +0700

    ktp

---
+++ src/app/(app)/penghuni/[id]/update-ktp/page.tsx
@@ -0,0 +1,10 @@
+import AddPenghuniForm from "@/components/AddPenghuniForm";
+import PhotoUploadForm from "@/components/PhotoUploadForm";
+
+export default function Page() {
+  return (
+    <div>
+      <PhotoUploadForm />
+    </div>
+  );
+}
